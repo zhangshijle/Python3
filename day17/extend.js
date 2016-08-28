5198926d715a659e833667e06abea11b921d6ac2
Zhang shijie <zhangshijie@zhangshijiedeMacBook-Pro.local>
2016-08-28 22:06:07 +0800

    表单验证、菜单和djando

---
+++ day17/extend.js
@@ -0,0 +1,10 @@
+/**
+ * Created by zhangshijie on 16/8/28.
+ */
+(function(jq){
+        jq.extend({
+            'myjs1':function(arg){
+                console.log(arg);
+            }
+        });
+})(jQuery);
